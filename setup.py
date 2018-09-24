031c14c4586506e5cc2732114a307a8068e9ee3d
Gabrielle McCormack <36075564+gmccorma@users.noreply.github.com>
2018-09-23 21:51:55 -0400

    created to fix travis-ci problem of failed import

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Attempt to fix problem with travis-ci not recognizing my imports.
+
+from setuptools import setup, find_packages
+
+setup(name='github_info',
+      author='Gabrielle McCormack',
+      packages=find_packages(),
+     )
